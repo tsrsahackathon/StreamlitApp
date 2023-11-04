9fb83aef26be8a2268d8de0a8dfcaa215a18d60f
tsrsahackathon <149416052+tsrsahackathon@users.noreply.github.com>
2023-11-04 10:40:23 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,28 +1,29 @@
 import streamlit as st
-import Retreive as rt
-
-
+from pymongo import MongoClient
+uri = "mongodb+srv://shriteqhackathon:5F5RZ96CiqZY4jzM@hackathon.vs7oa0j.mongodb.net/?retryWrites=true&w=majority"
+def get_latest_file_content():
+    client = MongoClient(uri)
+    db = client['Inscribd']
+    collection = db['Files']
+    latest_file = collection.find_one(sort=[('_id', -1)])
+    if latest_file:
+        file_content = latest_file.get('file_content')
+        if file_content:
+            return file_content
+    else:
+        return None
+    client.close()
 def home():
     st.title("Welcome to Inscribd")
-    # Add content for the home page
-# Page 2: Lectures
-
-
 def lectures():
     st.title("Lectures")
     st.markdown("## Transcription")
-    st.write(rt.get_latest_file_content())
-
-
-# Sidebar navigation
+    st.write(get_latest_file_content())
 pages = {
     "Home": home,
     "Lectures": lectures,
 }
-
 st.sidebar.title("Navigation")
 selection = st.sidebar.radio("Go to", list(pages.keys()))
-
-# Display the selected page
 page = pages[selection]
 page()
